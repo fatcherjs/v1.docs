fc43d3c2d824e0176936c5a445a4b36b5cc0999a
Fansy <35361626+fanhaoyuan@users.noreply.github.com>
2022-10-17 23:32:06 +0800

    refactor: move `v1 docs` to here (#1)

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+
+export default async () => {
+    const mdx = (await import('@mdx-js/rollup')).default;
+    const frontmatter = (await import('remark-frontmatter')).default;
+    const mdxFrontmatter = (await import('remark-mdx-frontmatter')).remarkMdxFrontmatter;
+
+    return defineConfig({
+        root: __dirname,
+        base: '/v1/docs/',
+        publicDir: path.resolve(__dirname, 'public'),
+        build: {
+            outDir: path.resolve(__dirname, './dist'),
+            emptyOutDir: true,
+        },
+        plugins: [
+            react(),
+            mdx({
+                providerImportSource: '@mdx-js/react',
+                remarkPlugins: [frontmatter, mdxFrontmatter],
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+            },
+        },
+        optimizeDeps: {
+            include: ['react/jsx-runtime'],
+        },
+    });
+};
